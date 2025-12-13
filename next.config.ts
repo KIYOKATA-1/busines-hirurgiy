ce8b1b33ed0beede37de127c9dc637f7f3f02986
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2025-12-13 10:40:17 +0500

    fix #6

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://businesssurgerybackend-dev-fa4d.up.railway.app/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
